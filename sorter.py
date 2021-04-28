36dbf29bbbe2465ebbf5cb80df64c0ed5c82c78a
alexo <alex-ong>
2021-04-29 08:48:55 +1000

    fmt: Blackify code

--- sorter.py
+++ sorter.py
@@ -9,26 +9,27 @@ def try_int(item):
         return int(item)
     except:
         return None
-        
+
+
 legit_lines = []
 current_item = None
 with open("input.txt", encoding="utf-8") as f:
     for line in f:
         line = line.strip()
         items = line.split()
-        
+
         if len(items) > 0:
             # mark current item.
             first = try_int(items[0])
             if first is None:
-                current_item = line                
+                current_item = line
             else:
                 item_count = str(first)
                 match = item_query.search(line)
                 string_idx, ikea_id = match.start(), match.group()
                 desc_len = string_idx - len(str(item_count))
-                item_name = line[len(str(item_count)):desc_len].strip()
-                output_string = ",".join([ikea_id, item_count, item_name, current_item])                
+                item_name = line[len(str(item_count)) : desc_len].strip()
+                output_string = ",".join([ikea_id, item_count, item_name, current_item])
                 legit_lines.append(output_string)
 
 legit_lines.sort()
